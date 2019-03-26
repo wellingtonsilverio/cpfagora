5689a4a052c5da7853aef6f9466b35b7b1bb494d
Wellington Silverio <wellington.silverio@synvia.com>
2019-03-26 11:10:38 -0300

    <fix> add async await

--- routes/cpf.route.js
+++ routes/cpf.route.js
@@ -1,29 +1,23 @@
 const CPFModel = require('../models/cpf.model');
 const CNPJModel = require('../models/cnpj.model');
 
-const getCPF = (req, res) => {
+const getCPF = async (req, res) => {
     // Verifica se é CPF ou CNPJ
 
     const cpfcnpj = req.params.cpfcnpj;
 
-    const cpf = validarCPF(cpfcnpj);
+    const cpf = await validarCPF(cpfcnpj);
+    console.log({cpf});
     if (cpf) {
         CPFModel.findOne({ cpf })
-            .then((_cpf) => {
+            .then(async (_cpf) => {
                 if(_cpf) {
                     res.send(_cpf);
                 } else {
-                    _cpf = new CPFModel({
-                        cpf: cpf
+                    const data = await CPFModel.create({
+                        cpf
                     });
-                    _cpf.save(error => {
-                        if (error) {
-                            console.log("error save cpf: ", error);
-                            res.send(error);
-                        } else {
-                            res.send(error || _cpf);
-                        }
-                    });
+                    res.send(data);
                 }
             })
             .catch((error) => {
@@ -31,24 +25,18 @@ const getCPF = (req, res) => {
                 res.send(error);
             });
     } else {
-        const cnpj = validarCNPJ(cpfcnpj);
+        const cnpj = await validarCNPJ(cpfcnpj);
+        console.log({cnpj});
         if (cnpj) {
             CNPJModel.findOne({ cnpj })
-            .then((_cnpj) => {
+            .then(async (_cnpj) => {
                 if(_cnpj) {
                     res.send(_cnpj);
                 } else {
-                    _cnpj = new CPFModel({
-                        cnpj: cnpj
+                    const data = await CNPJModel.create({
+                        cnpj
                     });
-                    _cnpj.save(error => {
-                        if (error) {
-                            console.log("error save cnpj: ", error);
-                            res.send(error);
-                        } else {
-                            res.send(error || _cnpj);
-                        }
-                    });
+                    res.send(data);
                 }
             })
             .catch((error) => {
@@ -89,92 +77,96 @@ module.exports = {
     getCPF
 };
 
-function validarCPF(cpf) {
-    cpf = cpf.replace(/[^\d]+/g, '');
-    if (cpf == '') return false;
-    // Elimina CPFs invalidos conhecidos	
-    if (cpf.length != 11 ||
-        cpf == "00000000000" ||
-        cpf == "11111111111" ||
-        cpf == "22222222222" ||
-        cpf == "33333333333" ||
-        cpf == "44444444444" ||
-        cpf == "55555555555" ||
-        cpf == "66666666666" ||
-        cpf == "77777777777" ||
-        cpf == "88888888888" ||
-        cpf == "99999999999")
-        return false;
-    // Valida 1o digito	
-    add = 0;
-    for (i = 0; i < 9; i++)
-        add += parseInt(cpf.charAt(i)) * (10 - i);
-    rev = 11 - (add % 11);
-    if (rev == 10 || rev == 11)
-        rev = 0;
-    if (rev != parseInt(cpf.charAt(9)))
-        return false;
-    // Valida 2o digito	
-    add = 0;
-    for (i = 0; i < 10; i++)
-        add += parseInt(cpf.charAt(i)) * (11 - i);
-    rev = 11 - (add % 11);
-    if (rev == 10 || rev == 11)
-        rev = 0;
-    if (rev != parseInt(cpf.charAt(10)))
-        return false;
-
-    return cpf;
+const validarCPF = (cpf) => {
+    return new Promise((resolve) => {
+        cpf = cpf.replace(/[^\d]+/g, '');
+        if (cpf == '') return resolve(false);
+        // Elimina CPFs invalidos conhecidos	
+        if (cpf.length != 11 ||
+            cpf == "00000000000" ||
+            cpf == "11111111111" ||
+            cpf == "22222222222" ||
+            cpf == "33333333333" ||
+            cpf == "44444444444" ||
+            cpf == "55555555555" ||
+            cpf == "66666666666" ||
+            cpf == "77777777777" ||
+            cpf == "88888888888" ||
+            cpf == "99999999999")
+            return resolve(false);
+        // Valida 1o digito	
+        add = 0;
+        for (i = 0; i < 9; i++)
+            add += parseInt(cpf.charAt(i)) * (10 - i);
+        rev = 11 - (add % 11);
+        if (rev == 10 || rev == 11)
+            rev = 0;
+        if (rev != parseInt(cpf.charAt(9)))
+            return resolve(false);
+        // Valida 2o digito	
+        add = 0;
+        for (i = 0; i < 10; i++)
+            add += parseInt(cpf.charAt(i)) * (11 - i);
+        rev = 11 - (add % 11);
+        if (rev == 10 || rev == 11)
+            rev = 0;
+        if (rev != parseInt(cpf.charAt(10)))
+            return resolve(false);
+    
+        return resolve(cpf);
+    }); 
 }
 
-function validarCNPJ(cnpj) {
-    cnpj = cnpj.replace(/[^\d]+/g, '');
+const validarCNPJ = (cnpj) => {
+    return new Promise((resolve) => {
+        cnpj = cnpj.replace(/[^\d]+/g, '');
 
-    if (cnpj == '') return false;
-
-    if (cnpj.length != 14)
-        return false;
-
-    // Elimina CNPJs invalidos conhecidos
-    if (cnpj == "00000000000000" ||
-        cnpj == "11111111111111" ||
-        cnpj == "22222222222222" ||
-        cnpj == "33333333333333" ||
-        cnpj == "44444444444444" ||
-        cnpj == "55555555555555" ||
-        cnpj == "66666666666666" ||
-        cnpj == "77777777777777" ||
-        cnpj == "88888888888888" ||
-        cnpj == "99999999999999")
-        return false;
-
-    // Valida DVs
-    tamanho = cnpj.length - 2
-    numeros = cnpj.substring(0, tamanho);
-    digitos = cnpj.substring(tamanho);
-    soma = 0;
-    pos = tamanho - 7;
-    for (i = tamanho; i >= 1; i--) {
-        soma += numeros.charAt(tamanho - i) * pos--;
-        if (pos < 2)
-            pos = 9;
-    }
-    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-    if (resultado != digitos.charAt(0))
-        return false;
-
-    tamanho = tamanho + 1;
-    numeros = cnpj.substring(0, tamanho);
-    soma = 0;
-    pos = tamanho - 7;
-    for (i = tamanho; i >= 1; i--) {
-        soma += numeros.charAt(tamanho - i) * pos--;
-        if (pos < 2)
-            pos = 9;
-    }
-    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-    if (resultado != digitos.charAt(1))
-        return false;
-
-    return cnpj;
+        if (cnpj == '') return resolve(false);
+    
+        if (cnpj.length != 14)
+            return resolve(false);
+    
+        // Elimina CNPJs invalidos conhecidos
+        if (cnpj == "00000000000000" ||
+            cnpj == "11111111111111" ||
+            cnpj == "22222222222222" ||
+            cnpj == "33333333333333" ||
+            cnpj == "44444444444444" ||
+            cnpj == "55555555555555" ||
+            cnpj == "66666666666666" ||
+            cnpj == "77777777777777" ||
+            cnpj == "88888888888888" ||
+            cnpj == "99999999999999")
+            return resolve(false);
+    
+        // Valida DVs
+        tamanho = cnpj.length - 2
+        numeros = cnpj.substring(0, tamanho);
+        digitos = cnpj.substring(tamanho);
+        soma = 0;
+        pos = tamanho - 7;
+        for (i = tamanho; i >= 1; i--) {
+            soma += numeros.charAt(tamanho - i) * pos--;
+            if (pos < 2)
+                pos = 9;
+        }
+        resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+        if (resultado != digitos.charAt(0))
+            return resolve(false);
+    
+        tamanho = tamanho + 1;
+        numeros = cnpj.substring(0, tamanho);
+        soma = 0;
+        pos = tamanho - 7;
+        for (i = tamanho; i >= 1; i--) {
+            soma += numeros.charAt(tamanho - i) * pos--;
+            if (pos < 2)
+                pos = 9;
+        }
+        resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+        if (resultado != digitos.charAt(1))
+            return resolve(false);
+        
+        return resolve(cnpj);
+    });
 }
