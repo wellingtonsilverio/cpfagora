215972123513ce858be69fa86ccd99eb485050e1
Wellington Silverio <wellington.silverio@synvia.com>
2019-04-08 09:01:41 -0300

    <refator> funcao principal

--- routes/cpfcnpj.route.js
+++ routes/cpfcnpj.route.js
@@ -10,9 +10,78 @@ const moment = require('moment');
 const CONTROLLER = 1;
 
 const getCPF = async (req, res) => {
-    // Verifica se é CPF ou CNPJ
-    const cpfcnpj = req.params.cpfcnpj;
+    await checkCpfOrCnpj(req.params.cpfcnpj);
+};
 
+module.exports = {
+    getCPF
+};
+
+const getCPFofCPFCNPJ = (CPFCNPJ_KEY, cpf, callback) => {
+    request({
+        url:  `${env.CPFCNPJ_API}/${CPFCNPJ_KEY}/7/json/${cpf}`,
+        timeout: 1000
+    }, async (error, resp) => {
+        if (error) {
+            console.log({ error });
+            if (error.code == "ETIMEDOUT" || error.code == "ESOCKETTIMEDOUT") console.log("-------------------------ETIMEDOUT-------------------------");
+            callback(error, null);
+        } else if (resp && resp.statusCode == 200) {
+            const cpfcnpj = {
+                ...JSON.parse(resp.body),
+                cpf
+            };
+
+            cpfcnpj.nascimento = moment(cpfcnpj.nascimento, "DD-MM-YYYY", "America/Sao_Paulo").toISOString();
+
+            cpfcnpj.pacoteUsado = undefined;
+            cpfcnpj.saldo = undefined;
+            cpfcnpj.consultaID = undefined;
+            cpfcnpj.delay = undefined;
+
+            callback(null, cpfcnpj);
+        } else callback({
+            statusCode: resp.statusCode,
+            error: "CPF não encontrado ou conexão com o DB falhou."
+        }, null);
+    });
+}
+
+const getCNPJofReceitaws = (cnpj, callback) => {
+    request(`${env.RECEITAWS_API}/${cnpj}`, async (error, resp) => {
+        if (error) {
+            console.log({ error });
+            callback(error, null);
+        }
+        if (resp && resp.statusCode == 200) {
+            const receitaws = {
+                ...JSON.parse(resp.body),
+                cnpj
+            };
+
+            receitaws.data_situacao = moment(receitaws.data_situacao, "DD-MM-YYYY", "America/Sao_Paulo").toISOString();
+            receitaws.abertura = moment(receitaws.abertura, "DD-MM-YYYY", "America/Sao_Paulo").toISOString();
+            if (receitaws.atividade_principal.length > 0) receitaws.atividade_principal = await receitaws.atividade_principal.map(atividades => {
+                return {
+                    name: atividades.text,
+                    code: atividades.code
+                }
+            });
+            if (receitaws.atividades_secundarias.length > 0) receitaws.atividades_secundarias = await receitaws.atividades_secundarias.map(atividades => {
+                return {
+                    name: atividades.text,
+                    code: atividades.code
+                }
+            });
+            callback(null, receitaws);
+        } else callback({
+            statusCode: resp.statusCode,
+            error: "CNPJ não encontrado ou conexão com o DB falhou."
+        }, null);
+    });
+}
+
+const checkCpfOrCnpj = async (cpfcnpj) => {
     const cpf = await validate.validarCPF(cpfcnpj);
     if (cpf) {
         CPFModel.findOne({ cpf })
@@ -62,83 +131,4 @@ const getCPF = async (req, res) => {
             response.failure(res, CONTROLLER, 6, { error: "CPF/CNPJ invalido." });
         }
     }
-};
-
-module.exports = {
-    getCPF
-};
-
-
-/**
- * Get CPF in CPFCNPJ API and format this
- * 
- * @param  {{any, any}} callback - return {error, cpfcnpj}
- */
-const getCPFofCPFCNPJ = (CPFCNPJ_KEY, cpf, callback) => {
-    request({
-        url:  `${env.CPFCNPJ_API}/${CPFCNPJ_KEY}/7/json/${cpf}`,
-        timeout: 1000
-    }, async (error, resp) => {
-        if (error) {
-            console.log({ error });
-            if (error.code == "ETIMEDOUT" || error.code == "ESOCKETTIMEDOUT") console.log("-------------------------ETIMEDOUT-------------------------");
-            callback(error, null);
-        } else if (resp && resp.statusCode == 200) {
-            const cpfcnpj = {
-                ...JSON.parse(resp.body),
-                cpf
-            };
-
-            cpfcnpj.nascimento = moment(cpfcnpj.nascimento, "DD-MM-YYYY", "America/Sao_Paulo").toISOString();
-
-            cpfcnpj.pacoteUsado = undefined;
-            cpfcnpj.saldo = undefined;
-            cpfcnpj.consultaID = undefined;
-            cpfcnpj.delay = undefined;
-
-            callback(null, cpfcnpj);
-        } else callback({
-            statusCode: resp.statusCode,
-            error: "CPF não encontrado ou conexão com o DB falhou."
-        }, null);
-    });
-}
-
-/**
- * Get CNPJ in Receitaws API and format this
- * 
- * @param  {{any, any}} callback - return {error, receitaws}
- */
-const getCNPJofReceitaws = (cnpj, callback) => {
-    request(`${env.RECEITAWS_API}/${cnpj}`, async (error, resp) => {
-        if (error) {
-            console.log({ error });
-            callback(error, null);
-        }
-        if (resp && resp.statusCode == 200) {
-            const receitaws = {
-                ...JSON.parse(resp.body),
-                cnpj
-            };
-
-            receitaws.data_situacao = moment(receitaws.data_situacao, "DD-MM-YYYY", "America/Sao_Paulo").toISOString();
-            receitaws.abertura = moment(receitaws.abertura, "DD-MM-YYYY", "America/Sao_Paulo").toISOString();
-            if (receitaws.atividade_principal.length > 0) receitaws.atividade_principal = await receitaws.atividade_principal.map(atividades => {
-                return {
-                    name: atividades.text,
-                    code: atividades.code
-                }
-            });
-            if (receitaws.atividades_secundarias.length > 0) receitaws.atividades_secundarias = await receitaws.atividades_secundarias.map(atividades => {
-                return {
-                    name: atividades.text,
-                    code: atividades.code
-                }
-            });
-            callback(null, receitaws);
-        } else callback({
-            statusCode: resp.statusCode,
-            error: "CNPJ não encontrado ou conexão com o DB falhou."
-        }, null);
-    });
 }
