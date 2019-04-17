44bb41b44cca3563101fbc000b11831fc70f1e53
Wellington Silverio <wellington.silverio@synvia.com>
2019-04-17 10:14:08 -0300

    <feat> add route /

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -2,6 +2,10 @@ import { Express } from "express";
 import { getCPFOrCNPJ } from "../routes/search.route";
 
 export const routes = (app: Express) => {
+	app.route('/').get(
+		(req: any, res: any) => {
+			res.send("0.0.1");
+		});
 	app.route('/search/:cpfcnpj').get(getCPFOrCNPJ);
 	app.route('/search/:_user/:cpfcnpj').get(getCPFOrCNPJ);
 };
